e1a06103b881d1a2cf3d80d34d0807b8aefe7c7d
Vinnie Davies <vsantino@att.net>
2020-02-01 16:44:29 -0500

    Added attack functionality for player

---
+++ Assets/Scripts/Player/PlayerCombat.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public Transform attackpoint;
+
+    public float attackRange = 0.5f;
+
+    public LayerMask enemyLayers;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && animator.GetBool("IsJumping") == false)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        //Play attack animation
+        animator.SetTrigger("Attack");
+        //Detect enemies in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackpoint.position, attackRange, enemyLayers);
+        //Damage enemies
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("We hit " + enemy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackpoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackpoint.position, attackRange);
+    }
+}
