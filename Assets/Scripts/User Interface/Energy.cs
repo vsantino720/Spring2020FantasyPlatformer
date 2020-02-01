b3374b8227e58302d8fe9964cbaa21c427ad19c5
Imoh Oton <imohroton@gmail.com>
2020-02-01 18:32:59 -0500

    UI Update

---
+++ Assets/Scripts/User Interface/Energy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    public int energy;
+    public int numEP;
+
+    public Image[] energyPoints;
+    public Sprite fullEP;
+    public Sprite emptyEP;
+
+    void Update()
+    {
+        for (int i = 0; i < energyPoints.Length; i++)
+        {
+            if (i < energy)
+            {
+                energyPoints[i].sprite = fullEP;
+            }
+            else
+            {
+                energyPoints[i].sprite = emptyEP;
+            }
+
+            if (i < numEP)
+            {
+                energyPoints[i].enabled = true;
+            }
+            else
+            {
+                energyPoints[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/User Interface/Health.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numHP;
+
+    public Image[] healthPoints;
+    public Sprite fullHP;
+    public Sprite emptyHP;
+
+    void Update()
+    {
+        for (int i = 0; i < healthPoints.Length; i++)
+        {
+            if (i < health)
+            {
+                healthPoints[i].sprite = fullHP;
+            }
+            else
+            {
+                healthPoints[i].sprite = emptyHP;
+            }
+
+            if (i < numHP)
+            {
+                healthPoints[i].enabled = true;
+            } else
+            {
+                healthPoints[i].enabled = false;
+            }
+        }
+    }
+}
